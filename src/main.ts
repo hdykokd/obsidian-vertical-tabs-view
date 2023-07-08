cc47490f47c918553721d97be2973928bc4d93c7
hdykokd <hdykokd@gmail.com>
2023-07-09 01:09:18 +0900

    tabs list view

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { log } from './util/message';
 import { VerticalTabsViewSettings, DEFAULT_SETTINGS, VerticalTabsViewSettingTab } from './setting';
+import { VerticalTabsViewView, VIEW_TYPE_VERTICAL_TABS } from './view';
 
 export default class VerticalTabsView extends Plugin {
   settings: VerticalTabsViewSettings;
@@ -9,6 +10,10 @@ export default class VerticalTabsView extends Plugin {
     log('loading...');
     await this.loadSettings();
     this.addSettingTab(new VerticalTabsViewSettingTab(this.app, this));
+    this.app.workspace.onLayoutReady(async () => {
+      this.registerViewExtension();
+    });
+
     log('loaded.');
   }
 
@@ -22,5 +27,27 @@ export default class VerticalTabsView extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
+  }
+
+  registerViewExtension() {
+    this.registerView(VIEW_TYPE_VERTICAL_TABS, (leaf) => {
+      return new VerticalTabsViewView(leaf);
+    });
+    this.addCommand({
+      id: 'vertical-tabs-view-show',
+      name: 'Show vertical tabs view',
+      editorCallback: async () => {
+        const leaves = this.app.workspace.getLeavesOfType(VIEW_TYPE_VERTICAL_TABS);
+        if (leaves[0]) {
+          return this.app.workspace.revealLeaf(leaves[0]);
+        }
+        const leaf = this.app.workspace.getRightLeaf(false);
+        await leaf.setViewState({
+          type: VIEW_TYPE_VERTICAL_TABS,
+          active: true,
+        });
+        this.app.workspace.revealLeaf(leaf);
+      },
+    });
   }
 }
---
+++ src/view.ts
@@ -0,0 +1,112 @@
+import { ItemView, MarkdownView, setIcon, WorkspaceLeaf } from 'obsidian';
+
+const VIEW_PREFIX = 'vertical-tabs-view';
+const VIEW_CONTAINER_ID = VIEW_PREFIX + '-container';
+const VIEW_CONTENT_ID = VIEW_PREFIX + '-content';
+
+export const VIEW_TYPE_VERTICAL_TABS = 'view-type-vertical-tabs-view';
+
+export class VerticalTabsViewView extends ItemView {
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+    this.registerEvent(
+      this.app.workspace.on('layout-change', () => {
+        this.updateView();
+      }),
+    );
+    this.registerEvent(
+      this.app.workspace.on('active-leaf-change', (leaf) => {
+        if (!leaf) return;
+        const state = leaf.getViewState();
+        if (state.type !== 'markdown') return;
+        this.updateView();
+      }),
+    );
+  }
+
+  getViewType() {
+    return VIEW_TYPE_VERTICAL_TABS;
+  }
+
+  getIcon() {
+    // FIXME
+    return 'file-type';
+  }
+
+  getDisplayText() {
+    return 'vertical tabs view';
+  }
+
+  async onClose() {
+    this.containerEl.empty();
+  }
+
+  async onOpen() {
+    const el = this.contentEl;
+    el.id = VIEW_CONTENT_ID;
+
+    const messageContainer = document.createElement('div');
+    messageContainer.id = VIEW_CONTAINER_ID;
+
+    this.updateView();
+  }
+
+  updateView() {
+    const el = document.querySelector(`#${VIEW_CONTENT_ID}`);
+    if (!el) return;
+    el.setChildrenInPlace([]);
+
+    const ul = document.createElement('ul');
+    ul.className = 'vertical-tabs-view-list';
+
+    const leaves = this.app.workspace.getLeavesOfType('markdown');
+
+    leaves.forEach((leaf) => {
+      const listItem = document.createElement('li');
+      listItem.className = 'vertical-tabs-view-list-item';
+      const activeLeaf = this.app.workspace.getMostRecentLeaf();
+      // @ts-expect-error
+      if (activeLeaf && activeLeaf.id === leaf.id) {
+        listItem.className += ' focused';
+      }
+      listItem.onclick = () => {
+        this.app.workspace.setActiveLeaf(leaf);
+      };
+
+      // close button
+      const closeBtn = document.createElement('div');
+      closeBtn.className = 'vertical-tabs-view-list-item-close-btn';
+      setIcon(closeBtn, 'x');
+      closeBtn.onclick = () => {
+        leaf.detach();
+      };
+      listItem.appendChild(closeBtn);
+
+      const listItemNameContainer = document.createElement('div');
+      listItemNameContainer.className = 'vertical-tabs-view-list-item-name-container';
+
+      const dirname = document.createElement('span');
+      dirname.className = 'vertical-tabs-view-list-item-dirname';
+      // @ts-expect-error
+      dirname.innerText = leaf.view.file.parent.path;
+      const title = document.createElement('span');
+      title.className = 'vertical-tabs-view-list-item-title';
+      // @ts-expect-error
+      title.innerText = leaf.view.file.name.split('.').slice(0, -1);
+      listItemNameContainer.setChildrenInPlace([dirname, title]);
+
+      listItem.appendChild(listItemNameContainer);
+
+      // if (leaf.pinned) {
+      //   const pinBtn = document.createElement('div');
+      //   pinBtn.className = 'vertical-tabs-view-list-item-pin-btn';
+      //   setIcon(pinBtn, 'pin');
+      //   listItem.appendChild(pinBtn);
+      // }
+
+      ul.appendChild(listItem);
+    });
+
+    el.appendChild(ul);
+  }
+}
